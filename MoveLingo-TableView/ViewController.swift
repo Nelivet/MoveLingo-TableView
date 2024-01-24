f8bd654e8943c178808f39f6df21871032ae6b78
Ivette Fernández <ivettefernandezrodriguez@gmail.com>
2024-01-24 18:03:34 -0500

    table view function

--- MoveLingo-TableView/ViewController.swift
+++ MoveLingo-TableView/ViewController.swift
@@ -7,11 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  
+    
+    
+    @IBOutlet var table : UITableView!
+    
+    var models = ["first","second","third","fourth"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table.delegate = self
+        table.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row]
+        cell.textLabel?.font = UIFont(name: "Arial", size: 22)
+        return cell
     }
 
 
