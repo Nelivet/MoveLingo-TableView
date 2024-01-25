67503ee722e2c62a4cf3a81b977ce47783eb1db6
Ivette Fernández <ivettefernandezrodriguez@gmail.com>
2024-01-24 19:32:14 -0500

    implementation

--- MoveLingo-TableView/ViewController.swift
+++ MoveLingo-TableView/ViewController.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
   
-    
+    //
     
     @IBOutlet var table : UITableView!
     
-    var models = ["first","second","third","fourth"]
+    var models = ["English","Spanish","Portuguese","Chinese"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,10 +29,25 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = models[indexPath.row]
-        cell.textLabel?.font = UIFont(name: "Arial", size: 22)
+        cell.textLabel?.font = UIFont(name: "Arial", size: 23)
         return cell
+        
     }
-
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        models.swapAt(sourceIndexPath.row, destinationIndexPath.row)
+    }
+    
+    @IBAction func didTapSort() {
+        if table.isEditing{
+            table.isEditing = false
+        }
+        else {table.isEditing = true
+            
+        }
+    }
 
 }
 
